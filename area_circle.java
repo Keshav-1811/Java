5849b8ee1129ec4af7b9714c10cb61f843493f40
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-11 23:05:12 +0530

    first area

---
+++ area_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class area_circle {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter r:");
+        int r=sc.nextInt();
+        System.out.println(3.14*r*r);
+    }
+}
