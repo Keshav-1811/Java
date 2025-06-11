4e3095d0d2f6c39599331eb6a1edd09ddb81f8f8
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-11 22:11:29 +0530

    first variable

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+        String str="Hello";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(str);
+    }
+    
+}
