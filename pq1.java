4c52c3a01fd01719526a8e9c15dce22ff6d67126
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-12 07:16:13 +0530

    First avg

---
+++ pq1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pq1 {
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first Number:");
+        float a=sc.nextFloat();
+        System.out.println("Enter second Number:");
+        float b=sc.nextFloat();
+        System.out.println("Enter third Number:");
+        float c=sc.nextFloat();
+        System.out.println((a+b+c)/3);
+
+    }
+}
