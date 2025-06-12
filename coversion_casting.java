e50220512507f145d9bb1bbc3fbcca44d684a454
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-12 07:06:30 +0530

    First conversion

---
+++ coversion_casting.java
@@ -0,0 +1,11 @@
+public class coversion_casting {
+    public static void main(String args[]){
+        int a=22;         //type conversion
+        float b =a;       //destination type>= source type
+        System.out.println(b);
+        float c=22.55f;
+        int d=(int)c;
+        System.out.println(d);                                              
+    }
+    
+}
