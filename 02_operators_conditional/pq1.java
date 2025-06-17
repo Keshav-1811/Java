62836d848d947441b4792fedf0d979796383c1f9
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-17 21:58:02 +0530

    pq1

---
+++ 02_operators_conditional/pq1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pq1 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n=sc.nextInt();
+        if(n<0){
+            System.out.println("Negative");
+            
+        }else{
+            System.out.println("Positve");
+        }
+    }
+}
