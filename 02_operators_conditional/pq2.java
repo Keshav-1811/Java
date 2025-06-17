5b25579172db4b6ebd4e88b3ac80424560cfbaad
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-17 22:06:59 +0530

    fever detector

---
+++ 02_operators_conditional/pq2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class pq2 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n=sc.nextInt();
+        if(n<100){
+            System.out.println("No fever");
+            
+        }else{
+            System.out.println("Fever");
+        }
+    }
+}
