ca0750c5f1d367df97ce92328ebf49b31ae1db5f
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-19 11:20:01 +0530

    binomial

--- 04_function/binomial_coeff.java
+++ 04_function/binomial_coeff.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class factorial {
+public class binomial_coeff {
     public static long fact(int n){
         long f=1;
         for(int i=1;i<=n;i++){
@@ -7,11 +7,19 @@ public class factorial {
         }
         return f;
     }
+    public static long bin(int n, int r){
+        long a=(long)fact(n);
+        long b=(long)fact(r);
+        long c=(long)fact(n-r);
+        return (long)a/(b*c);
+    }
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter n: ");
         int n=sc.nextInt();
-        System.out.println("The factorial of "+n+" is "+fact(n));
+        System.out.println("Enter r: ");
+        int r=sc.nextInt();
+        System.out.println("The binomial coefficient is "+bin(n,r));
         sc.close();
     }
 }
