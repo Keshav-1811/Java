0e17ba3cd33feb0ba03dae79000cb75707833ae8
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-19 11:34:50 +0530

    overloading

---
+++ 04_function/overloading.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class overloading {
+    public static int mul(int a,int b){
+        return a*b;
+    }
+    public static float mul(float a,float b){
+        return a*b;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a=sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b=sc.nextInt();
+        System.out.println("Sum of "+a+" and "+b+" is "+mul(a,b));
+        sc.close();
+    }
+}
