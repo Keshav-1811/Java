17664c4c44615aea57052cc6680b554ed3898233
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-19 12:12:53 +0530

    Prime

---
+++ 04_function/prime.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class prime {
+    public static boolean isPrime(int a) {
+        if (a <= 1){ 
+            return false;
+        }
+        if (a == 2){
+            return true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        if(isPrime(n)){
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not Prime");
+        }
+    }
+}
