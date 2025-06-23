d9ce59cff6f4d63617e0dd8e5ac8614bfebe2a8e
Keshav-1811 <keshavkumargola9@gmail.com>
2025-06-23 11:32:40 +0530

    math methods

---
+++ 04_function/pq5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class pq5 {
+    public static void main(String[] args){
+        System.out.println(Math.pow(234,4));
+        System.out.println(Math.min(20,16));
+        System.out.println(Math.max(20,16));
+        System.out.println((float)Math.sqrt(23));
+        System.out.println((float)Math.abs(-1123.33));
+    }
+}
